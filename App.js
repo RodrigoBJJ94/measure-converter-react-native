78d2866c0937049d9ef9568d71845b91d956441a
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-11-20 14:19:43 -0300

    Version 1.15

--- App.js
+++ App.js
@@ -1392,249 +1392,249 @@ export default function App() {
     };
 
     const resultMass = (value) => {
-      if (massOne === 'squareMeter' && massTwo === 'squareMile') {
+      if (massOne === 'kilogram' && massTwo === 'tonne') {
         if (value !== '') {
-          setMassResult((Number(value) / 2590000).toFixed(1));
+          setMassResult((Number(value) / 1000).toFixed(3));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareMeter' && massTwo === 'squareYard') {
+      } else if (massOne === 'kilogram' && massTwo === 'milligram') {
         if (value !== '') {
-          setMassResult((Number(value) * 1.196).toFixed(1));
+          setMassResult((Number(value) * 1000000).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareMeter' && massTwo === 'squareFoot') {
+      } else if (massOne === 'kilogram' && massTwo === 'microgram') {
         if (value !== '') {
-          setMassResult((Number(value) * 10.764).toFixed(1));
+          setMassResult((Number(value) * 1000000000).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareMeter' && massTwo === 'squareInch') {
+      } else if (massOne === 'kilogram' && massTwo === 'pound') {
         if (value !== '') {
           setMassResult((Number(value) * 1550).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareMeter' && massTwo === 'hectare') {
+      } else if (massOne === 'kilogram' && massTwo === 'once') {
         if (value !== '') {
-          setMassResult((Number(value) / 10000).toFixed(4));
+          setMassResult((Number(value) * 35.274).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareMile' && massTwo === 'acre') {
+      } else if (massOne === 'tonne' && massTwo === 'kilogram') {
         if (value !== '') {
-          setMassResult((Number(value) * 640).toFixed(1));
+          setMassResult((Number(value) * 1000).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareYard' && massTwo === 'squareKilometer') {
+      } else if (massOne === 'tonne' && massTwo === 'gram') {
         if (value !== '') {
-          setMassResult((Number(value) / 1196000).toFixed(6));
+          setMassResult((Number(value) * 1000000).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareYard' && massTwo === 'squareMeter') {
+      } else if (massOne === 'tonne' && massTwo === 'milligram') {
         if (value !== '') {
-          setMassResult((Number(value) / 1.196).toFixed(1));
+          setMassResult((Number(value) * 1000000000).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareYard' && massTwo === 'squareMile') {
+      } else if (massOne === 'tonne' && massTwo === 'microgram') {
         if (value !== '') {
-          setMassResult((Number(value) / 3098000).toFixed(6));
+          setMassResult((Number(value) * 1000000000000).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareYard' && massTwo === 'squareFoot') {
+      } else if (massOne === 'tonne' && massTwo === 'pound') {
         if (value !== '') {
-          setMassResult((Number(value) * 9).toFixed(1));
+          setMassResult((Number(value) * 2205).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareYard' && massTwo === 'squareInch') {
+      } else if (massOne === 'tonne' && massTwo === 'once') {
         if (value !== '') {
-          setMassResult((Number(value) * 1296).toFixed(1));
+          setMassResult((Number(value) * 35274).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareYard' && massTwo === 'hectare') {
+      } else if (massOne === 'gram' && massTwo === 'tonne') {
         if (value !== '') {
-          setMassResult((Number(value) / 11960).toFixed(4));
+          setMassResult((Number(value) / 1000000).toFixed(6));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareYard' && massTwo === 'acre') {
+      } else if (massOne === 'gram' && massTwo === 'kilogram') {
         if (value !== '') {
-          setMassResult((Number(value) / 4840).toFixed(4));
+          setMassResult((Number(value) / 1000).toFixed(3));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareFoot' && massTwo === 'squareKilometer') {
+      } else if (massOne === 'gram' && massTwo === 'milligram') {
         if (value !== '') {
-          setMassResult((Number(value) / 10760000).toFixed(7));
+          setMassResult((Number(value) * 1000).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareFoot' && massTwo === 'squareMeter') {
+      } else if (massOne === 'gram' && massTwo === 'microgram') {
         if (value !== '') {
-          setMassResult((Number(value) / 10.764).toFixed(1));
+          setMassResult((Number(value) * 1000000).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareFoot' && massTwo === 'squareMile') {
+      } else if (massOne === 'gram' && massTwo === 'pound') {
         if (value !== '') {
-          setMassResult((Number(value) / 27880000).toFixed(7));
+          setMassResult((Number(value) / 454).toFixed(3));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareFoot' && massTwo === 'squareYard') {
+      } else if (massOne === 'gram' && massTwo === 'once') {
         if (value !== '') {
-          setMassResult((Number(value) / 9).toFixed(1));
+          setMassResult((Number(value) / 28.35).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareFoot' && massTwo === 'squareInch') {
+      } else if (massOne === 'milligram' && massTwo === 'tonne') {
         if (value !== '') {
-          setMassResult((Number(value) * 144).toFixed(1));
+          setMassResult((Number(value) / 1000000000).toFixed(9));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareFoot' && massTwo === 'hectare') {
+      } else if (massOne === 'milligram' && massTwo === 'kilogram') {
         if (value !== '') {
-          setMassResult((Number(value) / 107639).toFixed(6));
+          setMassResult((Number(value) / 1000000).toFixed(6));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareFoot' && massTwo === 'acre') {
+      } else if (massOne === 'milligram' && massTwo === 'gram') {
         if (value !== '') {
-          setMassResult((Number(value) / 43560).toFixed(5));
+          setMassResult((Number(value) / 1000).toFixed(3));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareInch' && massTwo === 'squareKilometer') {
+      } else if (massOne === 'milligram' && massTwo === 'microgram') {
         if (value !== '') {
-          setMassResult((Number(value) / 1550000000).toFixed(9));
+          setMassResult((Number(value) * 1000).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareInch' && massTwo === 'squareMeter') {
+      } else if (massOne === 'milligram' && massTwo === 'pound') {
         if (value !== '') {
-          setMassResult((Number(value) / 1550).toFixed(4));
+          setMassResult((Number(value) / 453592).toFixed(5));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareInch' && massTwo === 'squareMile') {
+      } else if (massOne === 'milligram' && massTwo === 'once') {
         if (value !== '') {
-          setMassResult((Number(value) / 4014000000).toFixed(9));
+          setMassResult((Number(value) / 28350).toFixed(5));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareInch' && massTwo === 'squareYard') {
+      } else if (massOne === 'microgram' && massTwo === 'tonne') {
         if (value !== '') {
-          setMassResult((Number(value) / 1296).toFixed(4));
+          setMassResult((Number(value) / 1000000000000).toFixed(12));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareInch' && massTwo === 'squareFoot') {
+      } else if (massOne === 'microgram' && massTwo === 'kilogram') {
         if (value !== '') {
-          setMassResult((Number(value) / 144).toFixed(3));
+          setMassResult((Number(value) / 1000000000).toFixed(9));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareInch' && massTwo === 'hectare') {
+      } else if (massOne === 'microgram' && massTwo === 'gram') {
         if (value !== '') {
-          setMassResult((Number(value) / 15500000).toFixed(7));
+          setMassResult((Number(value) / 1000000).toFixed(6));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'squareInch' && massTwo === 'acre') {
+      } else if (massOne === 'microgram' && massTwo === 'milligram') {
         if (value !== '') {
-          setMassResult((Number(value) / 6273000).toFixed(6));
+          setMassResult((Number(value) / 1000).toFixed(3));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'hectare' && massTwo === 'squareKilometer') {
+      } else if (massOne === 'microgram' && massTwo === 'pound') {
         if (value !== '') {
-          setMassResult((Number(value) / 100).toFixed(2));
+          setMassResult((Number(value) / 453600000).toFixed(8));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'hectare' && massTwo === 'squareMeter') {
+      } else if (massOne === 'microgram' && massTwo === 'once') {
         if (value !== '') {
-          setMassResult((Number(value) * 10000).toFixed(2));
+          setMassResult((Number(value) / 28350000).toFixed(7));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'hectare' && massTwo === 'squareMile') {
+      } else if (massOne === 'pound' && massTwo === 'tonne') {
         if (value !== '') {
-          setMassResult((Number(value) / 259).toFixed(3));
+          setMassResult((Number(value) / 2205).toFixed(4));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'hectare' && massTwo === 'squareYard') {
+      } else if (massOne === 'pound' && massTwo === 'kilogram') {
         if (value !== '') {
-          setMassResult((Number(value) * 11960).toFixed(1));
+          setMassResult((Number(value) / 2.205).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'hectare' && massTwo === 'squareFoot') {
+      } else if (massOne === 'pound' && massTwo === 'gram') {
         if (value !== '') {
-          setMassResult((Number(value) * 107639).toFixed(1));
+          setMassResult((Number(value) * 454).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'hectare' && massTwo === 'squareInch') {
+      } else if (massOne === 'pound' && massTwo === 'milligram') {
         if (value !== '') {
-          setMassResult((Number(value) * 15500000).toFixed(1));
+          setMassResult((Number(value) * 453592).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'hectare' && massTwo === 'acre') {
+      } else if (massOne === 'pound' && massTwo === 'microgram') {
         if (value !== '') {
-          setMassResult((Number(value) * 2.471).toFixed(1));
+          setMassResult((Number(value) * 453600000).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'acre' && massTwo === 'squareKilometer') {
+      } else if (massOne === 'pound' && massTwo === 'once') {
         if (value !== '') {
-          setMassResult((Number(value) / 247).toFixed(3));
+          setMassResult((Number(value) * 16).toFixed(3));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'acre' && massTwo === 'squareMeter') {
+      } else if (massOne === 'once' && massTwo === 'tonne') {
         if (value !== '') {
-          setMassResult((Number(value) * 4047).toFixed(1));
+          setMassResult((Number(value) / 35274).toFixed(5));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'acre' && massTwo === 'squareMile') {
+      } else if (massOne === 'once' && massTwo === 'kilogram') {
         if (value !== '') {
-          setMassResult((Number(value) / 640).toFixed(3));
+          setMassResult((Number(value) / 35.274).toFixed(2));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'acre' && massTwo === 'squareYard') {
+      } else if (massOne === 'once' && massTwo === 'gram') {
         if (value !== '') {
-          setMassResult((Number(value) * 4840).toFixed(1));
+          setMassResult((Number(value) * 28.35).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'acre' && massTwo === 'squareFoot') {
+      } else if (massOne === 'once' && massTwo === 'milligram') {
         if (value !== '') {
-          setMassResult((Number(value) * 43560).toFixed(1));
+          setMassResult((Number(value) * 28350).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'acre' && massTwo === 'squareInch') {
+      } else if (massOne === 'once' && massTwo === 'microgram') {
         if (value !== '') {
-          setMassResult((Number(value) * 6273000).toFixed(1));
+          setMassResult((Number(value) * 28350000).toFixed(1));
         } else {
           setMassResult('');
         }
-      } else if (massOne === 'acre' && massTwo === 'hectare') {
+      } else if (massOne === 'once' && massTwo === 'pound') {
         if (value !== '') {
-          setMassResult((Number(value) / 2.471).toFixed(1));
+          setMassResult((Number(value) / 16).toFixed(1));
         } else {
           setMassResult('');
         }
@@ -1646,7 +1646,7 @@ export default function App() {
         }
       } else {
         if (value !== '') {
-          setMassResult((Number(value) / 1000000).toFixed(6));
+          setMassResult((Number(value) * 1000).toFixed(1));
         } else {
           setMassResult('');
         };
